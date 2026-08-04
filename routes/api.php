ec6cdb9e4d8b52826532f3e8b506617b6c42b78d
João Paulo Novais <jp.trabalho@gmail.com>
2026-08-03 21:24:36 -0300

    bug: diagnostico

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Rota pública de diagnóstico
+Route::get('/ping', function () {
+    return response()->json([
+        'status' => 'ok',
+        'cron_secret_set' => !empty(env('CRON_SECRET')),
+        'db' => env('DB_CONNECTION'),
+    ]);
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
