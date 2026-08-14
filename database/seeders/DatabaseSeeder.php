cef91743ceeb5443aa7618c55f9352e23976ced2
João Paulo Novais <jp.trabalho@gmail.com>
2026-08-14 14:20:39 -0300

    bug: reconstrucao do banco em proudcao

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,33 +17,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Família Silva'
         ]);
 
-        // Cria usuários solicitados com as respectivas senhas
-        User::create([
-            'name' => 'Pai',
-            'email' => 'pai@email.com',
-            'password' => Hash::make('localhost@1'),
-            'role' => UserRole::PAI,
-            'family_id' => $family->id,
-            'balance' => 0
-        ]);
-
-        User::create([
-            'name' => 'Mãe',
-            'email' => 'mae@email.com',
-            'password' => Hash::make('localhost@1'),
-            'role' => UserRole::MAE,
-            'family_id' => $family->id,
-            'balance' => 0
-        ]);
-
-        User::create([
-            'name' => 'Filha',
-            'email' => 'filha@email.com',
-            'password' => Hash::make('localhost@1'),
-            'role' => UserRole::FILHA,
-            'family_id' => $family->id,
-            'balance' => 50
-        ]);
+        // (Os usuários padrão foram removidos conforme solicitado, permitindo o registro manual)
 
         $this->call([
             MissionSeeder::class,
