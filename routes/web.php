6079cbe86dda056af2888e65529323f60eecf386
João Paulo Novais <jp.trabalho@gmail.com>
2026-08-14 15:51:38 -0300

    feat: retirada de rota temporaria

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,6 @@ Route::get('/dashboard', function () {
 Route::get('/invite/{code}', [\App\Http\Controllers\InviteController::class, 'show']);
 Route::post('/invite/{code}/accept', [\App\Http\Controllers\InviteController::class, 'accept']);
 
-Route::get('/run-fresh-migration', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate:fresh', [
-        '--force' => true,
-        '--seed' => true
-    ]);
-    return 'Banco de dados recriado e populado com sucesso!';
-});
-
 require __DIR__.'/auth.php';
+
 
